6dc82fb657fc909f65461cbe69a2cbf24b89c1df
Stian Bakkane <stianba@gmail.com>
2018-09-18 13:30:48 +0200

    fix

--- schemas/schema.js
+++ schemas/schema.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import createSchema from 'part:@sanity/base/schema-creator';
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 import { settings } from '@ludens-reklame/klosser-theme-[theme]';
-import Schemas from '@ludens-reklame/klosser/sanity';
+import Schemas from '@ludens-reklame/klosser/dist/sanity';
 
 const schemas = new Schemas();
 
