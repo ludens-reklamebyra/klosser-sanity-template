fa9084e490d6cb79cee1540758f73dd633bd83d2
Stian Bakkane <stianba@gmail.com>
2018-09-18 13:09:29 +0200

    .

--- install.js
+++ install.js
@@ -1,57 +1,65 @@
 #!/usr/bin/env node
 const inquirer = require('inquirer');
+const replace = require('replace-in-file');
+const cmd = require('node-cmd');
+
+const asyncCMD = util.promisify(cmd.get);
 
 (async function init() {
-  const answers = await inquirer.prompt([
-    {
-      name: 'name',
-      type: 'input',
-      message: 'What is the Sanity name?',
-      validate: function(value) {
-        if (value.length) {
-          return true;
-        } else {
-          return 'Please enter a Sanity name';
+  try {
+    const answers = await inquirer.prompt([
+      {
+        name: 'name',
+        type: 'input',
+        message: 'What is the Sanity name?',
+        validate: function(value) {
+          if (value.length) {
+            return true;
+          } else {
+            return 'Please enter a Sanity name';
+          }
+        }
+      },
+      {
+        name: 'projectId',
+        type: 'input',
+        message: 'What is the Sanity project id?',
+        validate: function(value) {
+          if (value.length) {
+            return true;
+          } else {
+            return 'Please enter a Sanity project id';
+          }
+        }
+      },
+      {
+        name: 'theme',
+        type: 'input',
+        message: "What is the theme you're using?",
+        validate: function(value) {
+          if (value.length) {
+            return true;
+          } else {
+            return 'Please enter a theme name';
+          }
         }
       }
-    },
-    {
-      name: 'projectId',
-      type: 'input',
-      message: 'What is the Sanity project id?',
-      validate: function(value) {
-        if (value.length) {
-          return true;
-        } else {
-          return 'Please enter a Sanity project id';
-        }
-      }
-    },
-    {
-      name: 'theme',
-      type: 'input',
-      message: "What is the theme you're using?",
-      validate: function(value) {
-        if (value.length) {
-          return true;
-        } else {
-          return 'Please enter a theme name';
-        }
-      }
-    }
-  ]);
+    ]);
 
-  console.log({ answers });
-})();
+    await asyncCMD(`yarn add @ludens-reklame/klosser-theme-${answers.theme}`);
 
-async function readAndEdit(opts) {
-  let fileData = await readFile(opts.read);
+    await replace({
+      files: 'sanity.json',
+      from: '[projectId]',
+      to: answers.projectId
+    });
 
-  if (opts.replaceItems && opts.replaceItems.length) {
-    for (let replaceItem of opts.replaceItems) {
-      fileData = fileData.replace(replaceItem.key, replaceItem.value);
-    }
+    await replace({
+      files: 'schemas/schema.js',
+      from: '[theme]',
+      to: answers.theme
+    });
+  } catch (error) {
+    console.error(error);
   }
-
-  await writeFile(opts.write, fileData);
-}
+})();
